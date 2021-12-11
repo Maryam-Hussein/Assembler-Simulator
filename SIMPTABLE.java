5b1ef3e70f1821522fa8ffc113e9b778650efe01
MaryamHussien <76852264+MaryamHussien@users.noreply.github.com>
2021-12-11 13:29:30 +0200

    Update SIMPTABLE.java

--- SIMPTABLE.java
+++ SIMPTABLE.java
@@ -3,12 +3,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 
-public class SIMPTABLE extends OPTABLE
-{
-    static void printTwo() throws Exception
-    {
+public class SIMPTABLE {
+    static void printTwo() throws Exception {
         // Hash map act as database
-        HashMap<String, String> A = new HashMap<String, String>(); // A is name of Hash map
+        HashMap<String, String> A = new HashMap<String, String>(); // A is name of Hash map of instruction
         A.put("LDX", "04");
         A.put("LDCH", "50");
         A.put("STCH", "54");
@@ -26,48 +24,49 @@ public class SIMPTABLE extends OPTABLE
 
         BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\HUAWEI\\IdeaProjects\\EC 241 - Assmbler\\src\\com\\company\\input.txt"));
         String text;
-        while ((text = bufferedReader.readLine()) != null)
-        {
+        int LOC = -3;
+        while ((text = bufferedReader.readLine()) != null) {
             String[] ABC = text.split("\\s");
-
             for (int i = 1; i < ABC.length; i++) // ABC is name of Array of Strings --> input.txt
             {
-                if (ABC[i].equals("LDX")) {
-                    SIMPTABLE LDX = new SIMPTABLE();
-                    System.out.println(A.get("LDX")+"           "+LDX);
-                }
-                else if (ABC[i].equals("LDCH")) {
-                    SIMPTABLE LDCH = new SIMPTABLE();
-                    System.out.println(A.get("LDCH")+"           "+LDCH);
-                } else if (ABC[i].equals("STCH")) {
-                    SIMPTABLE STCH = new SIMPTABLE();
-                    System.out.println(A.get("STCH")+"           "+STCH);
-                } else if (ABC[i].equals("TIX")) {
-                    SIMPTABLE TIX = new SIMPTABLE();
-                    System.out.println(A.get("TIX")+"           "+TIX);
-                } else if (ABC[i].equals("JLT")) {
-                    SIMPTABLE JLT = new SIMPTABLE();
-                    System.out.println(A.get("JLT")+"           "+JLT);
-                } else if (ABC[i].equals("JGT")) {
-                    SIMPTABLE JGT = new SIMPTABLE();
-                    System.out.println(A.get("JGT")+"           "+JGT);
-                } else if (ABC[i].equals("JEQ")) {
-                    SIMPTABLE JEQ = new SIMPTABLE();
-                    System.out.println(A.get("JEQ")+"           "+JEQ);
-                } else if (ABC[i].equals("DIV")) {
-                    SIMPTABLE DIV = new SIMPTABLE();
-                    System.out.println(A.get("DIV")+"           "+DIV);
-                } else if (ABC[i].equals("MUL")) {
-                    SIMPTABLE MUL = new SIMPTABLE();
-                    System.out.println(A.get("MUL")+"           "+MUL);
-                } else if (ABC[i].equals("SUB")) {
-                    SIMPTABLE SUB = new SIMPTABLE();
-                    System.out.println(A.get("SUB")+"           "+SUB);
-                } else if (ABC[i].equals("ADD")) {
-                    SIMPTABLE ADD = new SIMPTABLE();
-                    System.out.println(A.get("ADD")+"           "+ADD);
+                    if (ABC[i].equals("LDX")) {
+                        LOC+=3;
+                        System.out.println(A.get("LDX") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("LDCH")) {
+                        LOC += 3;
+                        System.out.println(A.get("LDCH") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("STCH")) {
+                        LOC += 3;
+                        System.out.println(A.get("STCH") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("TIX")) {
+                        LOC += 3;
+                        System.out.println(A.get("TIX") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("JLT")) {
+                        LOC += 3;
+                        System.out.println(A.get("JLT") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("JGT")) {
+                        LOC += 3;
+                        System.out.println(A.get("JGT") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("JEQ")) {
+                        LOC += 3;
+                        System.out.println(A.get("JEQ") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("DIV")) {
+                        LOC += 3;
+                        System.out.println(A.get("DIV") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("MUL")) {
+                        LOC += 3;
+                        System.out.println(A.get("MUL") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("SUB")) {
+                        LOC += 3;
+                        System.out.println(A.get("SUB") + "           " + Integer.toHexString(LOC));
+                    } else if (ABC[i].equals("ADD")) {
+                        LOC += 3;
+                        System.out.println(A.get("ADD") + "           " + Integer.toHexString(LOC));
+                    }else if(ABC[i].equals("WORD")){LOC+=3;}
+                    else if(ABC[i].equals("BYTE")){LOC+=3;}
+                    else if(ABC[i].equals("RESB")){LOC+=3;}
+                    else if(ABC[i].equals("RESW")){LOC+=3;}
                 }
             }
         }
     }
-}
